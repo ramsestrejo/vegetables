ec731d0de73d38a5bd622469364e8bb9437e1f2e
ramsesmindx <ramses@mindextent.com>
2019-07-12 13:24:46 -0400

    Making GreenPepper class implement Organic

--- src/main/java/sheridan/vegetables/GreenPepper.java
+++ src/main/java/sheridan/vegetables/GreenPepper.java
@@ -9,14 +9,25 @@ package sheridan.vegetables;
  *
  * @author ramses
  */
-public class GreenPepper extends Vegetable {
+public class GreenPepper extends Vegetable implements Organic {
+    
+    private String farm;
     
     public GreenPepper( String name , String colour ) {
         super( name , colour );
     }
     
+    public GreenPepper( String name , String colour , String farm ) {
+        this( name , colour );
+        this.farm = farm;
+    }
+    
     public boolean isTasty( ) {
         return true;
+    }
+    
+    public String getCertifiedFarmName( ) {
+        return farm;
     }
     
 }
